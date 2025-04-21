a664e5db55d4b895b8802f88978a3759246b34e1
noorulimann <niman1690@gmail.com>
2025-04-21 11:06:37 +0500

    scd lab sess commit

--- client/src/api/PostRequest.js
+++ client/src/api/PostRequest.js
@@ -4,3 +4,7 @@ const API = axios.create({ baseURL: 'http://localhost:4000' });
 
 export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
 export const likePost = (id, userId) => API.put(`post/${id}/like_dislike`, { userId: userId })
+
+
+
+ 
